bee1a6ad319f5ee815793c625d63a00bc1e07797
Lea li <liyanhui82@gmail.com>
2020-05-22 16:25:50 -0700

    adapt to androidX for build

--- APPetit-master/APPetit/customer/src/main/java/com/mad/customer/UI/OrderingFragment.java
+++ APPetit-master/APPetit/customer/src/main/java/com/mad/customer/UI/OrderingFragment.java
@@ -1,15 +1,12 @@
 package com.mad.customer.UI;
 
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -31,7 +28,6 @@ import com.mad.mylibrary.Restaurateur;
 
 import java.util.ArrayList;
 
-import static com.facebook.FacebookSdk.getApplicationContext;
 import static com.mad.mylibrary.SharedClass.RESTAURATEUR_INFO;
 
 
